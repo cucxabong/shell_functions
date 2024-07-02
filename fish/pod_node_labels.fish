97e593deed9d50c81861f0c13ca61df3f9749918
Quan Hoang <hdquan@protonmail.com>
2024-07-02 10:17:09 +0100

    feat: Adding 'podIP' field

--- fish/pod_node_labels.fish
+++ fish/pod_node_labels.fish
@@ -1,5 +1,5 @@
-function pod_node_labels --description "Getting Pods and labels of Nodes which are hosting those Pods"
-  set pods $(kubectl get pods $argv -o json | jq -c '.items|map({nodeName: .spec.nodeName, podName: .metadata.name})')
+function pod_node_labels
+  set pods $(kubectl get pods $argv -o json | jq -c '.items|map({nodeName: .spec.nodeName, podName: .metadata.name, podIP: .status.podIP})')
   set nodes $(kubectl get nodes -o json | jq -c '.items|map(.metadata.labels+{nodeName: .metadata.name})')
-  jq -s '[.[0][] as $pod | (.[1][] | select($pod.nodeName == .nodeName) as $node | {podName: $pod.podName}+$node)]' (echo $pods | psub) (echo $nodes | psub)
+  jq -s '[.[0][] as $pod | (.[1][] | select($pod.nodeName == .nodeName) as $node | {podName: $pod.podName, podIP: $pod.podIP}+$node)]' (echo $pods | psub) (echo $nodes | psub)
 end
